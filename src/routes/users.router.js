4db7d7ab4c80764db0676c1298853ae89662cd9c
marcohzo <marcoherazo11@gmail.com>
2024-11-22 18:19:36 -0300

    Update users.router.js

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -6,15 +6,10 @@ import {
 } from "../controllers/user.controller.js";
 import authenticateJWT from "../middlewares/authenticateJWT.js";
 
-const createUsersRouter = async () => {
-  await initializeUsers();
-  const router = Router();
+const router = Router();
 
-  router.post("/register", registerUser);
-  router.post("/login", loginUser);
-  router.get("/current", authenticateJWT, getCurrentUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/current", authenticateJWT, getCurrentUser);
 
-  return router;
-};
-
-export default createUsersRouter;
+export default router;
